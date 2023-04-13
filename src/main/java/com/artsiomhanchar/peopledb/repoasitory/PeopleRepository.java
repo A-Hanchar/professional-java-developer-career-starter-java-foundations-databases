84573cdc2b7ed4541cb348784aedecc9dbe84341
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-04-13 18:02:09 +0400

    feat-init-project-and-add-first-test

---
+++ src/main/java/com/artsiomhanchar/peopledb/repoasitory/PeopleRepository.java
@@ -0,0 +1,9 @@
+package com.artsiomhanchar.peopledb.repoasitory;
+
+import com.artsiomhanchar.peopledb.model.Person;
+
+public class PeopleRepository {
+    public Person save(Person person) {
+        return person;
+    }
+}
