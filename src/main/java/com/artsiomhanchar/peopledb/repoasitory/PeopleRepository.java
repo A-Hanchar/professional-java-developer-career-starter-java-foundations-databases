42516f768d8b0c10c3617ccfb088521c1d674366
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-04-13 19:40:29 +0400

    feat-passed-lessons-201-202-203

--- src/main/java/com/artsiomhanchar/peopledb/repoasitory/PeopleRepository.java
+++ src/main/java/com/artsiomhanchar/peopledb/repoasitory/PeopleRepository.java
@@ -2,8 +2,45 @@ package com.artsiomhanchar.peopledb.repoasitory;
 
 import com.artsiomhanchar.peopledb.model.Person;
 
+import java.sql.*;
+import java.time.ZoneId;
+
 public class PeopleRepository {
+    public static final String SAVE_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES(?, ?, ?)";
+    private Connection connection;
+
+    public PeopleRepository(Connection connection) {
+        this.connection = connection;
+    }
+
     public Person save(Person person) {
+//        String sql = String.format(
+//                "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES('%s', '%s', '%s')",
+//                person.getFirstName(), person.getLastName(), person.getDob()
+//        );
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(SAVE_PERSON_SQL, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, person.getFirstName());
+            ps.setString(2, person.getLastName());
+            ps.setTimestamp(3, Timestamp.valueOf(person.getDob().withZoneSameInstant(ZoneId.of("+0")).toLocalDateTime()));
+
+            int recordsAffected = ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+
+            while (rs.next()) {
+                long id = rs.getLong(1);
+
+                person.setId(id);
+                System.out.println(person);
+            }
+
+            System.out.printf("Records affected: %d%n", recordsAffected);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return person;
     }
 }
